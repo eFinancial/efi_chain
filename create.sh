0f531ac04a340c85f0bb699ab28ea718a30c7a24
Stefan <stefan.treffeisen@campusjaeger.de>
2018-10-13 19:43:46 +0200

    Fixed files

--- create.sh
+++ create.sh
@@ -4,10 +4,10 @@ cryptogen generate --config=./crypto-config.yaml
 # create genesis block
 export FABRIC_CFG_PATH=$PWD
 mkdir channel-artifacts
-configtxgen -profile EfiChannel -outputBlock ./channel-artifacts/genesis.block
+configtxgen -profile EfiOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
 # create channel
-export CHANNEL_NAME=billChannel
+export CHANNEL_NAME=billchannel
 configtxgen -profile EfiChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
 
 # create peers
